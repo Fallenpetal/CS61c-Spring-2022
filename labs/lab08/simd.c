e20a0e69d01a89737fa07ba18ca2d9776071dd13
Fallenpetal <93207655+Fallenpetal@users.noreply.github.com>
2022-04-12 20:06:30 +0800

    lab8 done

---
+++ labs/lab08/simd.c
@@ -0,0 +1,133 @@
+#include <time.h>
+#include <stdio.h>
+#include <x86intrin.h>
+#include "simd.h"
+
+long long int sum(int vals[NUM_ELEMS]) {
+    clock_t start = clock();
+
+    long long int sum = 0;
+    for(unsigned int w = 0; w < OUTER_ITERATIONS; w++) {
+        for(unsigned int i = 0; i < NUM_ELEMS; i++) {
+            if(vals[i] >= 128) {
+                sum += vals[i];
+            }
+        }
+    }
+    clock_t end = clock();
+    printf("Time taken: %Lf s\n", (long double)(end - start) / CLOCKS_PER_SEC);
+    return sum;
+}
+
+long long int sum_unrolled(int vals[NUM_ELEMS]) {
+    clock_t start = clock();
+    long long int sum = 0;
+
+    for(unsigned int w = 0; w < OUTER_ITERATIONS; w++) {
+        for(unsigned int i = 0; i < NUM_ELEMS / 4 * 4; i += 4) {
+            if(vals[i] >= 128) sum += vals[i];
+            if(vals[i + 1] >= 128) sum += vals[i + 1];
+            if(vals[i + 2] >= 128) sum += vals[i + 2];
+            if(vals[i + 3] >= 128) sum += vals[i + 3];
+        }
+
+        // TAIL CASE, for when NUM_ELEMS isn't a multiple of 4
+        // NUM_ELEMS / 4 * 4 is the largest multiple of 4 less than NUM_ELEMS
+        // Order is important, since (NUM_ELEMS / 4) effectively rounds down first
+        for(unsigned int i = NUM_ELEMS / 4 * 4; i < NUM_ELEMS; i++) {
+            if (vals[i] >= 128) {
+                sum += vals[i];
+            }
+        }
+    }
+    clock_t end = clock();
+    printf("Time taken: %Lf s\n", (long double)(end - start) / CLOCKS_PER_SEC);
+    return sum;
+}
+
+long long int sum_simd(int vals[NUM_ELEMS]) {
+    clock_t start = clock();
+    __m128i _127 = _mm_set1_epi32(127); // This is a vector with 127s in it... Why might you need this?
+    long long int result = 0; // This is where you should put your final result!
+    /* DO NOT MODIFY ANYTHING ABOVE THIS LINE (in this function) */
+
+    for(unsigned int w = 0; w < OUTER_ITERATIONS; w++) {
+        /* YOUR CODE GOES HERE */
+        __m128i tmp = _mm_set1_epi32(0);
+        int res[4];     //res[]必须是int型，我依据返回类型定义成long long int ，因为两者具有不同位宽，所以error
+        unsigned int i = 0;
+        for(; i + 4 < NUM_ELEMS; i += 4) {
+          __m128i vector = _mm_loadu_si128((__m128i *)(vals + i));
+          __m128i mask = _mm_cmpgt_epi32(vector, _127);
+          __m128i gt127 = _mm_and_si128(vector, mask);
+          tmp = _mm_add_epi32(tmp, gt127);
+        }
+        _mm_storeu_si128((__m128i *)res, tmp);
+        /* Hint: you'll need a tail case. */
+          for (; i < NUM_ELEMS; i++) {
+            if (vals[i] >= 128) {
+              res[0] += vals[i];
+          }
+          }
+          for (int j = 0; j < 4; j++) {
+            result += res[j];
+          }
+    }
+
+    /* DO NOT MODIFY ANYTHING BELOW THIS LINE (in this function) */
+    clock_t end = clock();
+    printf("Time taken: %Lf s\n", (long double)(end - start) / CLOCKS_PER_SEC);
+    return result;
+}
+
+long long int sum_simd_unrolled(int vals[NUM_ELEMS]) {
+    clock_t start = clock();
+    __m128i _127 = _mm_set1_epi32(127);
+    long long int result = 0;
+    /* DO NOT MODIFY ANYTHING ABOVE THIS LINE (in this function) */
+
+    for(unsigned int w = 0; w < OUTER_ITERATIONS; w++) {
+        /* YOUR CODE GOES HERE */
+        /* Copy your sum_simd() implementation here, and unroll it */
+        __m128i tmp = _mm_set1_epi32(0);
+        int res[4];     
+        unsigned int i = 0;
+        for(; i + 16 < NUM_ELEMS; i += 16) {
+          __m128i vector = _mm_loadu_si128((__m128i *)(vals + i));
+          __m128i mask = _mm_cmpgt_epi32(vector, _127);
+          __m128i gt127 = _mm_and_si128(vector, mask);
+          tmp = _mm_add_epi32(tmp, gt127);
+
+          vector = _mm_loadu_si128((__m128i *)(vals + i + 4));
+          mask = _mm_cmpgt_epi32(vector, _127);
+          gt127 = _mm_and_si128(vector, mask);
+          tmp = _mm_add_epi32(tmp, gt127);
+
+          vector = _mm_loadu_si128((__m128i *)(vals + i + 8));
+          mask = _mm_cmpgt_epi32(vector, _127);
+          gt127 = _mm_and_si128(vector, mask);
+          tmp = _mm_add_epi32(tmp, gt127);
+
+          vector = _mm_loadu_si128((__m128i *)(vals + i + 12));
+          mask = _mm_cmpgt_epi32(vector, _127);
+          gt127 = _mm_and_si128(vector, mask);
+          tmp = _mm_add_epi32(tmp, gt127);
+        }
+        _mm_storeu_si128((__m128i *)res, tmp);
+
+          for (; i < NUM_ELEMS; i++) {
+            if (vals[i] >= 128) {
+              res[0] += vals[i];
+          }
+          }
+          for (int j = 0; j < 4; j++) {
+            result += res[j];
+          }
+        /* Hint: you'll need 1 or maybe 2 tail cases here. */
+    }
+
+    /* DO NOT MODIFY ANYTHING BELOW THIS LINE (in this function) */
+    clock_t end = clock();
+    printf("Time taken: %Lf s\n", (long double)(end - start) / CLOCKS_PER_SEC);
+    return result;
+}
